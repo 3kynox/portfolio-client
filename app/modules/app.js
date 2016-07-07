507fccd65b24a3b88de4d6c61514a45482a1a0ae
Thierry PROST <Thierry PROST>
2016-07-07 16:46:14 +0200

    Add & Configure RestAngular -> jSonLD -> Hydra Vocab

--- app/modules/app.js
+++ app/modules/app.js
@@ -13,6 +13,7 @@ var MakeApp = angular
       'oc.lazyLoad',
       'ui.router',
       'ui.bootstrap',
+      'restangular',
       'dashboardApp',
       'layoutApp',
       'pagesApp',
@@ -69,8 +70,51 @@ var MakeApp = angular
                 }
             }
         );
-    }
-]);
+    }])
+    .config(['RestangularProvider', function (RestangularProvider) {
+        // The URL of the API endpoint
+        RestangularProvider.setBaseUrl('http://localhost/portfolio/server/web/');
+
+        // JSON-LD @id support
+        RestangularProvider.setRestangularFields({
+            id: '@id'
+        });
+        RestangularProvider.setSelfLinkAbsoluteUrl(false);
+
+        // Hydra collections support
+        RestangularProvider.addResponseInterceptor(function (data, operation) {
+            // Remove trailing slash to make Restangular working
+            function populateHref(data) {
+                if (data['@id']) {
+                    data.href = data['@id'].substring(1);
+                }
+            }
+
+            // Populate href property for the collection
+            populateHref(data);
+
+            if ('getList' === operation) {
+                var collectionResponse = data['hydra:member'];
+                collectionResponse.metadata = {};
+
+                // Put metadata in a property of the collection
+                angular.forEach(data, function (value, key) {
+                    if ('hydra:member' !== key) {
+                        collectionResponse.metadata[key] = value;
+                    }
+                });
+
+                // Populate href property for all elements of the collection
+                angular.forEach(collectionResponse, function (value) {
+                    populateHref(value);
+                });
+
+                return collectionResponse;
+            }
+
+            return data;
+        });
+    }]);
 
 // Route State Load Spinner(used on page or content load)
 MakeApp.directive('ngSpinnerLoader', ['$rootScope',
